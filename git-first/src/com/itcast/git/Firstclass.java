af77a16d00d65ddcfb8f72f747bceb9eb04a7818
admin <1149802487@qq.com>
2019-10-15 10:26:16 +0800

    修改firstclass

--- git-first/src/com/itcast/git/Firstclass.java
+++ git-first/src/com/itcast/git/Firstclass.java
@@ -7,5 +7,12 @@ public class Firstclass {
         System.out.println("hello first git");
         System.out.println("hello first git");
         System.out.println("hello first git");
+
+    }
+    public static void test(){
+        int i=0;
+        i+=10;
+        System.out.println(i);
+
     }
 }
