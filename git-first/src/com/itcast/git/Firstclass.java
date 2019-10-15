0977de38b4e170b1fae541ef587eaf79f0a8d60e
admin <1149802487@qq.com>
2019-10-15 09:47:50 +0800

    本地工程添加git

---
+++ git-first/src/com/itcast/git/Firstclass.java
@@ -0,0 +1,11 @@
+package com.itcast.git;
+
+public class Firstclass {
+    public static void main(String[] args) {
+        System.out.println("hello first git");
+        System.out.println("hello first git");
+        System.out.println("hello first git");
+        System.out.println("hello first git");
+        System.out.println("hello first git");
+    }
+}
